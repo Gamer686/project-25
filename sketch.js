c18982d661a6bf76f8cd60a1f686f3bd92705032
Gamer686 <74534832+Gamer686@users.noreply.github.com>
2020-12-29 13:37:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -48,7 +48,7 @@ function draw() {
 }
 function keyPressed(){
 	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:11,y:-15});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:10,y:-18});
 
 	}
 }
