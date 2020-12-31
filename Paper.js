1d5f973a8cdd1406d96fb6c9550a31bdf3fcf4fe
Gamer686 <74534832+Gamer686@users.noreply.github.com>
2020-12-31 14:21:07 +0530

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -1,26 +1,27 @@
 class Paper{
-    constructor(){
+    constructor(x,y,radius){
         var options ={
-            
+            isStatic:false,
             restitution:0,
             friction:0,
             density:1.2
     }
 
-this.body = Bodies.circle(100,300,10,options);
-this.width = 33;
+this.body = Bodies.circle(100,300,radius,options);
+this.radius = radius*2;
 this.image = loadImage("paper.png");
 World.add(world,this.body);
 
    }
    display(){
        push()
+       translate
        var pos = this.body.position;
        var angle = this.body.angle;
        fill("White");
        rotate(angle);
        imageMode(CENTER);
-       image( this.image,pos.x,pos.y,this.width,this.width);
+       image( this.image,pos.x,pos.y,this.radius*2,this.radius*2);
        pop()
    }
 }
